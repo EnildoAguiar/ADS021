21e9827cf443cfee068f738456f96565e2b297ef
ENILDO AGUIAR <@192.168.25.3>
2019-11-29 12:16:49 -0200

    Correção na função de salvar

--- laravel/database/migrations/2019_09_13_185905_criar_tabela_moradores.php
+++ laravel/database/migrations/2019_09_13_185905_criar_tabela_moradores.php
@@ -13,7 +13,7 @@ class CriarTabelaMoradores extends Migration
      */
     public function up()
     {
-        Schema::create('moradores', function (Blueprint $table) {
+        Schema::create  ('moradores', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome');
             $table->integer('cpf');
